f0e0bb684d0f2b44c52e5de07cdfe0356c19cdbe
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-16 22:45:14 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 require "pry"
 def position_taken?(board, index)
   # binding.pry
-  if board[index] == " " || board[index] == ""
+  if board[index] == " " || board[index] == "" || board[index] == nil
    false
   end
 end
